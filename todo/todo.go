6b2cc5029f1c460c40399ac9a9d961a840a08fa7
Shreyas G Gowda <shreyasg2526@gmail.com>
2026-05-11 17:35:14 +0530

    Add initial commit

---
+++ todo/todo.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Lets complete the tasks!")
+	var task1 string = "Learn devops"
+	var task2 string = "Complete go programming"
+	const task3 int = 3
+
+	fmt.Printf("Try to %s and try to %s within %d days\n", task1, task2, task3)
+
+}
