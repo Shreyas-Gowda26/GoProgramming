570e382d1269cd2ffaaed7062e37e30c18309454
Shreyas G Gowda <shreyasg2526@gmail.com>
2026-05-11 17:37:54 +0530

    Add pointers

---
+++ pointers/pointers.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Pointers in go\n")
+	var name string = "Shreyas"
+	fname := &name
+	fmt.Println("Enter your name: ")
+	fmt.Scanln(&name)
+	fmt.Println("Hello", name)
+	fmt.Println("The value of fname is: ", *fname)
+}
