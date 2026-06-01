fc01303d7cd1a4a8a74462ae737c29469485e4c9
Shreyas G Gowda <shreyasg2526@gmail.com>
2026-06-01 23:42:56 +0530

    Add ArrayandSlices

--- ArrayandSlices/arrayslices.go
+++ ArrayandSlices/arrayslices.go
@@ -3,6 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	var arr = [3]string{"Shreyas", "Chinav", "Deekshith"}
+	var FirstName string
+	var LastName string
+
+	fmt.Println("Enter the FirstName: ")
+	fmt.Scanln(&FirstName)
+	fmt.Println("Enter the LastName: ")
+	fmt.Scanln(&LastName)
+
+	var arr = [3]string{}
 	fmt.Println(arr)
+	arr[0] = FirstName + " " + LastName
+	fmt.Printf("The whole array is: %v\n", arr)
+
+	var slice []string
+	slice = append(slice, "Shreyas")
+	fmt.Println(slice)
 }
