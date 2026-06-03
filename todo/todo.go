fb06c56fcc411cff79c6d6ee02a51d0dfc1197df
Shreyas G Gowda <shreyasg2526@gmail.com>
2026-06-03 23:47:28 +0530

    Add switch cases

--- todo/todo.go
+++ todo/todo.go
@@ -10,4 +10,23 @@ func main() {
 
 	fmt.Printf("Try to %s and try to %s within %d days\n", task1, task2, task3)
 
+	task := "writing"
+
+	switch task {
+	case "reading":
+		fmt.Println("You are reading a book")
+
+	case "swimming":
+		fmt.Println("You are in a pool")
+
+	case "running":
+		fmt.Println("You are running")
+
+	case "writing":
+		fmt.Println("You are writing")
+
+	default:
+		fmt.Println("You are doing nothing")
+	}
+
 }
